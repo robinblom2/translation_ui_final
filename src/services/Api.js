c49af4950319465b00ec97070dfc418f9dc05548
Robin Blom <robin.blom@hotmail.se>
2023-03-09 11:53:24 +0100

    Changed url for endpoints

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,24 +2,24 @@ import axios from 'axios';
 
 export default {
   async fetchNodes(locale) {
-    return await axios.get('/test/test?locale=' + locale);
+    return await axios.get('/test/api/admin/Nodes?locale=' + locale);
   },
   fetchSelectOptions() {
-    return axios.get('/test/api/Locale');
+    return axios.get('/test/api/admin/Locales');
   },
   updateNode(node) {
     const requestData = {
       ParentId: node.parentId,
       Name: node.name,
     };
-    return axios.put(`/test/api/Node/${node.id}`, requestData);
+    return axios.put(`/test/api/admin/Nodes/${node.id}`, requestData);
   },
   updateKey(key) {
     const requestData = {
       NodeId: key.nodeId,
       Name: key.name,
     };
-    return axios.put(`/test/api/Key/${key.id}`, requestData);
+    return axios.put(`/test/api/admin/Keys/${key.id}`, requestData);
   },
   updateTranslation(translation, keyId, locale) {
     const requestData = {
@@ -27,27 +27,30 @@ export default {
       LocaleId: locale,
       Value: translation.value,
     };
-    return axios.put(`/test/api/Translation/${translation.id}`, requestData);
+    return axios.put(
+      `/test/api/admin/Translations/${translation.id}`,
+      requestData
+    );
   },
   addKey(keyName, nodeId) {
     const requestData = {
       NodeId: nodeId,
       Name: keyName,
     };
-    return axios.post('/test/api/Key', requestData);
+    return axios.post('/test/api/admin/Keys', requestData);
   },
   deleteKey(keyId) {
-    return axios.delete(`/test/api/Key/${keyId}`);
+    return axios.delete(`/test/api/admin/Keys/${keyId}`);
   },
   addNode(nodeName, parentId) {
     const requestData = {
       ParentId: parentId,
       Name: nodeName,
     };
-    return axios.post('/test/api/Node', requestData);
+    return axios.post('/test/api/admin/Nodes', requestData);
   },
   deleteNode(nodeId) {
-    return axios.delete(`/test/api/Node/${nodeId}`);
+    return axios.delete(`/test/api/admin/Nodes/${nodeId}`);
   },
 
   // Locales
@@ -56,15 +59,15 @@ export default {
       Name: locale.name,
     };
     console.log(requestData);
-    return axios.post('/test/api/Locale', requestData);
+    return axios.post('/test/api/admin/Locales', requestData);
   },
   DeleteLocale(id) {
-    return axios.delete(`/test/api/Locale/${id}`);
+    return axios.delete(`/test/api/admin/Locales/${id}`);
   },
   UpdateLocale(locale) {
     const requestData = {
       Name: locale.name,
     };
-    return axios.put(`/test/api/Locale/${locale.id}`, requestData);
+    return axios.put(`/test/api/admin/Locales/${locale.id}`, requestData);
   },
 };
