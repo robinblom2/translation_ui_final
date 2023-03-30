bd5730261ca8cdba48bfa9b0f5e3e635c5faf2f6
Robin Blom <robin.blom@hotmail.se>
2023-03-30 17:17:35 +0200

    Did some style changes, mostly to the modals

--- src/components/HandleLocaleComponent.vue
+++ src/components/HandleLocaleComponent.vue
@@ -1,62 +1,69 @@
 <template>
   <div>
-    <button class="me-5" @click="openModal">Handle Locales</button>
+    <button class="locale-btn locale-btn-s2" @click="openModal">
+      Handle Locales
+    </button>
     <div class="modal" v-if="showModal" @click.self="closeModal">
-      <div class="modal-content">
-        <button type="button" class="close-button" @click="closeModal">
-          X
-        </button>
-        <div class="d-flex flex-column h-100">
-          <div class="mb-5">
-            <h2 class="mb-3">Create new Locale</h2>
-            <form @submit.prevent="createLocale">
-              <label for="locale-name">Locale Name: </label>
-              <input
-                type="text"
-                id="locale-name"
-                v-model="localeName"
-                required
-              />
-              <button type="submit">Create</button>
-            </form>
+      <div class="close-btn-container">
+        <p class="close-btn" @click="closeModal">X</p>
+      </div>
+      <div class="modal-bg">
+        <div class="modal-content">
+          <div class="d-flex justify-content-end">
+            <select v-model="selectedLocale">
+              <option value="">Select a locale...</option>
+              <option
+                v-for="option in localeStore.selectOptions"
+                :value="option"
+              >
+                {{ option.name }}
+              </option>
+            </select>
           </div>
-          <div>
-            <div class="d-flex align-items-center justify-content-between">
-              <h2 class="mb-3">Update or Delete Locale</h2>
-              <label for="locale-select">Choose a locale:</label>
-              <select id="locale-select" v-model="selectedLocale">
-                <option
-                  v-for="option in localeStore.selectOptions"
-                  :value="option"
-                >
-                  {{ option.name }}
-                </option>
-              </select>
-            </div>
-
-            <div v-if="updateSuccess" class="success-message">
-              You've successfully updated a locale.
-            </div>
-            <div v-if="deleteSuccess" class="delete-message">
-              You've successfully deleted a locale.
-            </div>
-            <div v-if="selectedLocale">
-              <form @submit.prevent="updateLocale(selectedLocale)">
-                <label for="locale-name">Locale Name:</label>
+          <div class="d-flex flex-column h-100">
+            <div class="mb-5">
+              <h2 class="mb-3">Add Locale</h2>
+              <form @submit.prevent="createLocale">
                 <input
                   type="text"
                   id="locale-name"
-                  v-model="selectedLocale.name"
+                  placeholder="Add new locale..."
+                  v-model="localeName"
                   required
                 />
-                <button type="submit">Update</button>
+                <button type="submit">Create</button>
               </form>
-              <button
-                @click.prevent="deleteLocale(selectedLocale.id)"
-                class="delete-button"
-              >
-                Delete
-              </button>
+            </div>
+
+            <div>
+              <div class="d-flex align-items-center justify-content-between">
+                <h2 class="mb-3">Update or Delete Locale</h2>
+              </div>
+
+              <div v-if="updateSuccess" class="success-message">
+                You've successfully updated a locale.
+              </div>
+              <div v-if="deleteSuccess" class="delete-message">
+                You've successfully deleted a locale.
+              </div>
+              <div v-if="selectedLocale">
+                <form @submit.prevent="updateLocale(selectedLocale)">
+                  <label for="locale-name">Locale Name:</label>
+                  <input
+                    type="text"
+                    id="locale-name"
+                    v-model="selectedLocale.name"
+                    required
+                  />
+                  <button type="submit">Update</button>
+                  <button
+                    @click.prevent="deleteLocale(selectedLocale.id)"
+                    class="delete-button"
+                  >
+                    Delete
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
@@ -140,21 +147,109 @@ export default {
 </script>
 
 <style scoped>
-.modal {
-  background-color: rgba(0, 0, 0, 0.5);
+.locale-btn {
+  padding: 10px 25px;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  background: transparent;
+  outline: none !important;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+}
+.locale-btn-s2 {
+  border: 2px solid rgb(255, 255, 255);
+  z-index: 1;
+  color: white;
+  border-radius: 3px;
+}
+.locale-btn-s2:after {
+  position: absolute;
+  content: '';
+  width: 0;
+  height: 100%;
+  top: 0;
+  left: 0;
+  direction: rtl;
+  z-index: -1;
+  background: rgb(255, 255, 255);
+  transition: all 0.3s ease;
+}
+.locale-btn-s2:hover {
+  color: rgb(0, 0, 0);
+}
+
+.locale-btn-s2:hover:after {
+  left: auto;
+  right: 0;
+  width: 100%;
+}
+.locale-btn-s2:active {
+  top: 2px;
+}
+
+.modal-bg {
   display: flex;
   align-items: center;
+  width: 600px;
+  height: 360px;
+  margin: auto;
+  position: relative;
+  border: 3px solid transparent;
+  color: #fff;
+  background: #000;
+  border-radius: 1em;
+  /* padding: 13% 23%; */
+}
+
+.modal-bg:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
+  margin: -5px;
+  border-radius: inherit;
+  background: linear-gradient(to right, red, orange);
+}
+.modal {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
   justify-content: center;
+  background-color: #000000da;
+  z-index: 2;
 }
 
 .modal-content {
+  background-color: rgb(0, 0, 0);
   padding: 2rem;
-  width: 600px;
-  height: 400px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  top: 50%;
+  left: 50%;
 }
 
-.close-button {
-  align-self: flex-end;
+.close-btn-container {
+  position: absolute;
+  top: 10px;
+  right: 20px;
+}
+.close-btn {
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  cursor: pointer;
 }
 
 .success-message {
